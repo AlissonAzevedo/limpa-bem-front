349c883d0a10dc172666e3a133975466aa25048b
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2023-02-19 23:29:39 -0300

    feat: add NavBar and AboutView

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <router-view/>
+  <div class="w-full flex flex-col">
+    <NavBar/>
+    <router-view/>
+  </div>
 </template>
 
 <script>
-
+import NavBar from "@/components/NavBar.vue"
 export default {
   name: 'App',
+  components:{
+    NavBar
+  }
 }
 </script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="w-full">
+    <!-- Banner -->
+    <section class="bg-blue-300 py-40">
+      <div class="container mx-auto text-center">
+        <h1 class="text-4xl text-white font-bold mb-2">
+          Serviços de Limpeza Profissional
+        </h1>
+        <p class="text-white mb-8">
+          Deixe a limpeza da sua casa ou escritório conosco
+        </p>
+        <a
+          href="#"
+          class="bg-white text-blue-500 font-bold py-2 px-4 rounded-full hover:bg-blue-500 hover:text-white"
+          >Agende agora</a
+        >
+      </div>
+    </section>
+
+    <!-- Sobre -->
+    <section class="bg-white py-20">
+      <div class="container mx-auto">
+        <h2 class="text-3xl text-center font-bold mb-8">Sobre Nós</h2>
+        <div class="flex flex-wrap -mx-4">
+          <div class="w-full md:w-1/2 px-4">
+            <p class="text-xl mb-4">
+              Nós somos uma empresa de limpeza profissional com mais de 10 anos
+              de experiência no mercado. Nosso objetivo é fornecer serviços de
+              limpeza de alta qualidade para nossos clientes, garantindo a
+              limpeza e a higienização de cada ambiente.
+            </p>
+          </div>
+          <div class="w-full md:w-1/2 px-4">
+            <p class="text-xl mb-4">
+              Nós utilizamos apenas produtos de limpeza de qualidade e
+              equipamentos modernos para garantir a eficiência de nossos
+              serviços. Além disso, nossos funcionários são treinados e
+              capacitados para realizar a limpeza de forma profissional e
+              cuidadosa, respeitando as necessidades e preferências de cada
+              cliente.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutView",
+};
+</script>
